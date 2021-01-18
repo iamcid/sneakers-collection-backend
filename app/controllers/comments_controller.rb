c7c17fcacdd58ededd6e08c158e4b156fa02fe14
iamcid <iamcid17@gmail.com>
2021-01-18 01:23:48 -0500

    adds methods to controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,41 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:show, :update, :destroy]
+
+    def index
+        @comments = Comment.all
+
+        render json: @comments
+    end
+
+    def show
+
+        render json: @comment
+    end
+
+    def create
+        @comment = Comment.create(comment_params)
+
+        render json: @comment
+    end
+
+    def update
+        @comment.update(comment_params)
+
+        render json: @comment
+    end
+
+    def destroy
+        @comment.delete
+
+        render json: {commentId: @comment.id}
+    end
+
+    private
+        def set_comment
+            @comment = Comment.find(params[:id])
+        end
+
+        def comment_params
+            params.require(:comment).permit(:size, :comment, :sneaker_id)
+        end
 end
--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,2 +1,41 @@
 class SneakersController < ApplicationController
+    before_action :set_sneaker, only: [:show, :update, :destroy]
+
+    def index
+        @sneakers = sneaker.all
+
+        render json: @sneakers
+    end
+
+    def show
+
+        render json: @sneaker
+    end
+
+    def create
+        @sneaker = sneaker.create(sneaker_params)
+
+        render json: @sneaker
+    end
+
+    def update
+        @sneaker.update(sneaker_params)
+
+        render json: @sneaker
+    end
+
+    def destroy
+        @sneaker.delete
+
+        render json: {sneakerId: @sneaker.id}
+    end
+
+    private
+        def set_sneaker
+            @sneaker = sneaker.find(params[:id])
+        end
+
+        def sneaker_params
+            params.require(:sneaker).permit(:colorway, :name, :brand, :price, :image)
+        end
 end
